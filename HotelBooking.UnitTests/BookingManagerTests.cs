47dee5755ca12a122533573c65dadf8630fe08e2
Kjell Schoke <kjell.schoke@outlook.com>
2024-02-28 12:38:10 +0100

    Moq setup

--- HotelBooking.UnitTests/BookingManagerTests.cs
+++ HotelBooking.UnitTests/BookingManagerTests.cs
@@ -1,8 +1,11 @@
 using System;
-using HotelBooking.Core;
+using System.Collections.Generic;
 using HotelBooking.UnitTests.Fakes;
 using Xunit;
 using System.Linq;
+using HotelBooking.Core;
+using HotelBooking.Infrastructure.Repositories;
+using Moq;
 
 
 namespace HotelBooking.UnitTests
@@ -10,14 +13,41 @@ namespace HotelBooking.UnitTests
     public class BookingManagerTests
     {
         private IBookingManager bookingManager;
-        IRepository<Booking> bookingRepository;
-
+        private Mock<IRepository<Booking>> fakeBookingRespository;
+        private Mock<IRepository<Room>> fakeRoomRespository;
+        
         public BookingManagerTests(){
+            fakeBookingRespository = new Mock<IRepository<Booking>>();
+            fakeRoomRespository = new Mock<IRepository<Room>>();
+            
             DateTime start = DateTime.Today.AddDays(10);
             DateTime end = DateTime.Today.AddDays(20);
-            bookingRepository = new FakeBookingRepository(start, end);
-            IRepository<Room> roomRepository = new FakeRoomRepository();
-            bookingManager = new BookingManager(bookingRepository, roomRepository);
+            
+            List<Room> roomsFake =
+            [
+                new Room { Id = 1, Description = "A" },
+                new Room { Id = 2, Description = "B" },
+                new Room { Id = 3, Description = "C" }
+            ];
+            
+            List<Booking> bookingsFake =
+            [
+                new Booking { Id = 1, StartDate = start, EndDate = end, IsActive = true, RoomId = 1 },
+                new Booking { Id = 2, StartDate = DateTime.Today.AddDays(-10), EndDate = DateTime.Today.AddDays(-5), IsActive = false, RoomId = 2 },
+                new Booking { Id = 3, StartDate = start, EndDate = end.AddDays(10), IsActive = true, RoomId = 3 }
+            ];
+            
+            fakeBookingRespository.Setup(x => x.GetAll()).Returns(() => bookingsFake);
+            fakeBookingRespository.Setup(x => x.Get(It.IsAny<int>())).Returns((int id) => 
+                bookingsFake.FirstOrDefault(b => b.Id == id));
+
+            
+            fakeRoomRespository.Setup(x => x.GetAll()).Returns(() => roomsFake);
+            fakeRoomRespository.Setup(x => x.Get(It.IsAny<int>())).Returns((int id) => 
+                roomsFake.FirstOrDefault(r => r.Id == id));
+            
+            
+            bookingManager = new BookingManager(fakeBookingRespository.Object, fakeRoomRespository.Object);
         }
 
         [Fact]
